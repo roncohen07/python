2849c125c4f334118c312a00f2302afc9b2148ee
roncohen07 <143311647+roncohen07@users.noreply.github.com>
2024-02-01 10:07:33 +0200

    Update kefel.py

--- kefel.py
+++ kefel.py
@@ -1,19 +1,8 @@
-import sys
+user_input = int(input("Enter a number from 1 to 10: "))
 
-def generate_multiplication_table(user_input):
-    if 1 <= user_input <= 10:
-        for i in range(1, 11):
-            result = user_input * i
-            print(f"{user_input} x {i} = {result}")
-    else:
-        print("Please enter a number from 1 to 10.")
-
-if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        try:
-            user_input = int(sys.argv[1])
-            generate_multiplication_table(user_input)
-        except ValueError:
-            print("Please provide a valid number.")
-    else:
-        print("Usage: python kefel.py <number>")
+if 1 <= user_input <= 10:
+    for i in range(1, 11):
+        result = user_input * i
+        print(f"{user_input} x {i} = {result}")
+else:
+    print("Please enter a number from 1 to 10.")
