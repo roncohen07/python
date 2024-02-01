817c0f6fe92c1cea787146af40ec49da61951116
roncohen07 <143311647+roncohen07@users.noreply.github.com>
2024-02-01 10:10:17 +0200

    Update kefel.py

--- kefel.py
+++ kefel.py
@@ -1,4 +1,10 @@
-user_input = int(input("Enter a number from 1 to 10: "))
+import sys
+
+if len(sys.argv) != 2:
+    print("Please provide a number from 1 to 10 as a command-line argument.")
+    sys.exit(1)
+
+user_input = int(sys.argv[1])
 
 if 1 <= user_input <= 10:
     for i in range(1, 11):
